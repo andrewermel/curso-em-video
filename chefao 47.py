6a60d657aba052a2661be95da7482026f0d8bae6
andrewermel <andrew.ermel@gmail.com>
2019-01-31 13:01:38 -0200

    desafio 47

---
+++ chefao 47.py
@@ -0,0 +1,9 @@
+# crie um programa que mostre na
+# tela todos os numeros pares que
+# estão no intervalo entre 1 e 50.
+
+# exercicio
+
+for c in range(2,51,2):
+    print(c, end=' ')
+print('fim')
